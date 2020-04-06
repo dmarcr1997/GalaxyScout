93c1c7a746bbd2a6ba84d50a3aa1eb2b82dcbd2e
Damon <dmarcr1997@gmail.com>
2020-04-06 17:38:19 -0500

    Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/sessions/new', to: 'sessions#new', as: 'signin'
+  post '/sessions', to: "session#create"
+
+  get '/users/new', to:'users#signup', as:'signup'
+  post '/users', to: 'user#create'
+  get '/users/:id', to:'user#show', as:'homepage'
+
+  resources :albumns
+  
+  resources :galaxies, only: [:index, :show, :new, :create]
+  
+  resources :solar_systems, only: [:index, :show, :new, :create]
+  
+  resources :planets, only: [:index, :show, :new, :create]
+  
 end
